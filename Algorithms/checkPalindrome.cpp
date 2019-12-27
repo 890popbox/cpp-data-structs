54e6ee704fd81acd85ae0c26784f6adc32123dc8
Chris <43688978+890popbox@users.noreply.github.com>
2019-12-26 18:13:06 -0800

    Check if the String is a Palindrome

---
+++ Algorithms/checkPalindrome.cpp
@@ -0,0 +1,24 @@
+//Short way to check if a string is a Palindrome
+
+#include <iostream> 
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+bool checkPalindrome(string str)
+	for (unsigned i = 0, j = unsigned(str.size() - 1); i < unsigned(str.size() / 2); i++, j--) {
+		//check if space from both i and j, we don't want spacing.
+		while (str.at(i) == ' ') { i++; }
+		while (str.at(j) == ' ') { j--; }
+		//check if it's equal or not, cap/low diff doesn't matter
+		if (tolower(str.at(i)) != tolower(str.at(j))) { return false; }
+    return true;
+}
+
+int main()
+{ 
+  cout << checkPalindrome("Hello") << endl; //0
+  cout << checkPalindrome("rac  eca  r") << endl; //1
+  return 0;
+}
